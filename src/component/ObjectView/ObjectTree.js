614d6a456bae9cdb0e73d277a2169db539ea89d6
qs <350709533@qq.com>
2016-08-24 14:48:33 +0800

    对象树

--- src/component/ObjectView/ObjectTree.js
+++ src/component/ObjectView/ObjectTree.js
@@ -12,38 +12,40 @@ class ObjectTree extends React.Component {
 
     render() {
         let num = 0;
+        let url = "/dist/img/thumbnails.png";
         let objectData = {
             showState:1,
             openState:2,
             children:[
-                {showState:0,openState:0,name:"111",images:"",children:[]}
-                , {showState:1,openState:1,name:"111",images:"",
+                {showState:0,openState:0,name:"111",images:url,event:1,children:[]}
+                , {showState:1,openState:1,name:"111",images:url,event:"",
                     children:[
-                        {showState:0,openState:0,name:"222",images:"",children:[]}
-                        , {showState:1,openState:1,name:"222",images:"",
+                        {showState:0,openState:0,name:"222",images:url,event:"",children:[]}
+                        , {showState:1,openState:1,name:"222",images:url,event:"",
                             children:[
-                                {showState:0,openState:0,name:"333",images:"",children:[]}
+                                {showState:0,openState:0,name:"333",images:url,event:"",children:[]}
+                            ]}
+                        , {showState:0,openState:0,name:"222",images:url,event:"",children:[]}
+                    ]}
+                , {showState:0,openState:0,name:"111",images:url,event:"",children:[]}
+                , {showState:1,openState:1,name:"111",images:url,event:"",
+                    children:[
+                        {showState:0,openState:0,name:"222",images:url,event:"",children:[]}
+                        , {showState:1,openState:1,name:"222",images:url,event:1,
+                            children:[
+                                {showState:0,openState:0,name:"333",images:url,event:"",children:[]}
                             ]}
                     ]}
-                , {showState:0,openState:0,name:"111",images:"",children:[]}
-                , {showState:1,openState:1,name:"111",images:"",
-                    children:[
-                        {showState:0,openState:0,name:"222",images:"",children:[]}
-                        , {showState:1,openState:1,name:"222",images:"",
-                            children:[
-                                {showState:0,openState:0,name:"333",images:"",children:[]}
-                            ]}
-                    ]}
-                , {showState:0,openState:0,name:"111",images:"",children:[]}
+                , {showState:0,openState:0,name:"111",images:url,event:"",children:[]}
             ]
         };
 
         let btn = (show)=>{
             //0图层及图层内的所有内容不可看，1可在舞台看见
             if(show === 0 ){
-                return <div className="btn f--hcc hide-btn" style={{ marginRight: num === 0 ? "6px" :num*20+"px" }}><span /></div>;
+                return <div className="btn f--hcc hide-btn"><span /></div>;
             }else{
-                return <div className="btn f--hcc show-btn" style={{ marginRight: num === 0 ? "6px" :num*20+"px" }}><span /></div>;
+                return <div className="btn f--hcc show-btn"><span /></div>;
             }
         };
 
@@ -62,17 +64,17 @@ class ObjectTree extends React.Component {
 
         let stageContent = (data)=>{
             num++;
-            return data.map(xxx);
+            let content = data.map(xxx);
+            num--;
+            return content;
         };
-
+        //Todo:active 选中状态，放在item-title后面，stage-title后面为选中舞台
         let xxx = (v,i)=>{
             return  <div className="item" key={i}>
-                        <div className="item-title f--h f--hlc">
+                        <div className="item-title f--h f--hlc" style={{ paddingLeft: num === 0 ? "28px" :num *20 + 22 +"px" }}>
                             { btn(v.showState) }
                             { icon(v.openState) }
-                            <div className="title-icon">
-                                <img src={v.images} />
-                            </div>
+                            <img src={v.images} />
                             <p>{v.name}</p>
                             {
                                 v.event
