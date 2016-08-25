7a89c40ce8a3561fbf02c9acca354a4d25f4e933
qs <350709533@qq.com>
2016-08-25 12:24:30 +0800

    对象树打开文件夹

--- src/component/ObjectView/ObjectTree.js
+++ src/component/ObjectView/ObjectTree.js
@@ -7,11 +7,38 @@ class ObjectTree extends React.Component {
         super(props);
         this.state = {
             nid : null
-            , openData : []
+            , openData : [1,3]
         };
         this.chooseBtn = this.chooseBtn.bind(this);
-        //this.openBtn = this.openBtn.bind(this);
-        //this.closeBtn = this.closeBtn.bind(this);
+        this.openBtn = this.openBtn.bind(this);
+        this.closeBtn = this.closeBtn.bind(this);
+        this.objectData = {
+            showState:1,
+            openState:2,
+            nid:1,
+            children:[
+                {showState:0,openState:0,name:"111",images:"/dist/img/thumbnails.png",event:1,nid:2,children:[]}
+                , {showState:1,openState:2,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:3,
+                    children:[
+                        {showState:0,openState:0,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:4,children:[]}
+                        , {showState:1,openState:1,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:5,
+                            children:[
+                                {showState:0,openState:0,name:"333",images:"/dist/img/thumbnails.png",event:"",nid:6,children:[]}
+                            ]}
+                        , {showState:0,openState:0,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:7,children:[]}
+                    ]}
+                , {showState:0,openState:0,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:8,children:[]}
+                , {showState:1,openState:1,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:9,
+                    children:[
+                        {showState:0,openState:0,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:10,children:[]}
+                        , {showState:1,openState:1,name:"222",images:"/dist/img/thumbnails.png",event:1,nid:11,
+                            children:[
+                                {showState:0,openState:0,name:"333",images:"/dist/img/thumbnails.png",event:"",nid:12,children:[]}
+                            ]}
+                    ]}
+                , {showState:0,openState:0,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:13,children:[]}
+            ]
+        }
     }
 
     chooseBtn(data){
@@ -20,55 +47,61 @@ class ObjectTree extends React.Component {
         })
     }
 
-    //openBtn(id){
-    //    let data = this.state.openData;
-    //    data.push(id);
-    //    console.log(data);
-    //    this.setState({
-    //        openData : data
-    //    });
-    //}
-    //
-    //closeBtn(id){
-    //    let data = this.state.openData;
-    //    let index = data.indexOf(id);
-    //    data.splice(index, 1);
-    //    console.log(data);
-    //    this.setState({
-    //        openData : data
-    //    })
-    //}
+    openBtn(event){
+        event.stopPropagation();
+        let id = event.currentTarget.getAttribute("data-nid");
+        let data = this.state.openData;
+        data.push(id);
+        this.setState({
+            openData : data
+        });
+        if( id == 1 ){
+            this.objectData.openState = 1;
+        }
+        else {
+            let fuc = ((v,i)=>{
+                if(v.nid == id){
+                    v.openState = 1;
+                    return;
+                }
+                if(v.children.length !== 0){
+                    v.children.map(fuc);
+                }
+            });
+            this.objectData.children.map(fuc);
+        }
+        //console.log(data);
+    }
+
+    closeBtn(event){
+        event.stopPropagation();
+        let id = event.currentTarget.getAttribute("data-nid");
+        let data = this.state.openData;
+        let index = data.indexOf(id);
+        data.splice(index, 1);
+        this.setState({
+            openData : data
+        });
+        if(id == 1){
+            this.objectData.openState = 2;
+        }
+        else {
+            let fuc = ((v,i)=>{
+                if(v.nid == id){
+                    v.openState = 2;
+                    return;
+                }
+                if(v.children.length !== 0){
+                    v.children.map(fuc);
+                }
+            });
+            this.objectData.children.map(fuc);
+        }
+        //console.log(data);
+    }
 
     render() {
         let num = 0;
-        let url = "/dist/img/thumbnails.png";
-        let objectData = {
-            showState:1,
-            openState:2,
-            nid:1,
-            children:[
-                {showState:0,openState:0,name:"111",images:url,event:1,nid:2,children:[]}
-                , {showState:1,openState:2,name:"111",images:url,event:"",nid:3,
-                    children:[
-                        {showState:0,openState:0,name:"222",images:url,event:"",nid:4,children:[]}
-                        , {showState:1,openState:1,name:"222",images:url,event:"",nid:5,
-                            children:[
-                                {showState:0,openState:0,name:"333",images:url,event:"",nid:6,children:[]}
-                            ]}
-                        , {showState:0,openState:0,name:"222",images:url,event:"",nid:7,children:[]}
-                    ]}
-                , {showState:0,openState:0,name:"111",images:url,event:"",nid:8,children:[]}
-                , {showState:1,openState:1,name:"111",images:url,event:"",nid:9,
-                    children:[
-                        {showState:0,openState:0,name:"222",images:url,event:"",nid:10,children:[]}
-                        , {showState:1,openState:1,name:"222",images:url,event:1,nid:11,
-                            children:[
-                                {showState:0,openState:0,name:"333",images:url,event:"",nid:12,children:[]}
-                            ]}
-                    ]}
-                , {showState:0,openState:0,name:"111",images:url,event:"",nid:13,children:[]}
-            ]
-        };
 
         let btn = (show)=>{
             //0图层及图层内的所有内容不可看，1可在舞台看见
@@ -79,16 +112,16 @@ class ObjectTree extends React.Component {
             }
         };
 
-        let icon = (open)=>{
+        let icon = (open, nid)=>{
             // 0是该图层下不包含任何内容，1有内容，但是内容收起来， 2有内容，且展开内容
             if(open === 0){
                 return null;
             }
             else if(open === 1){
-                return <span className="icon close-icon" />;
+                return <span className="icon close-icon" onClick={this.closeBtn.bind(this)} data-nid={nid} />;
             }
             else {
-                return <span className="icon open-icon"  />;
+                return <span className="icon open-icon" onClick={this.openBtn.bind(this)} data-nid={nid} />;
             }
         };
 
@@ -129,19 +162,19 @@ class ObjectTree extends React.Component {
         return (
             <div className="ObjectTree">
                 <div className="stage">
-                    <div className={$class("stage-title f--h f--hlc",{"active": objectData.nid == this.state.nid})}
-                         onClick={this.chooseBtn.bind(this,objectData.nid)}>
-                        { btn(objectData.showState) }
-                        { icon(objectData.openState, objectData.nid) }
+                    <div className={$class("stage-title f--h f--hlc",{"active": this.objectData.nid == this.state.nid})}
+                         onClick={this.chooseBtn.bind(this,this.objectData.nid)}>
+                        { btn(this.objectData.showState) }
+                        { icon(this.objectData.openState, this.objectData.nid) }
                         <span className="stage-icon" />
                         <p>舞台</p>
                     </div>
 
-                    <div className="stage-content">
+                    <div className={$class("stage-content", {"hidden": this.objectData.openState === 0 || this.objectData.openState === 1 })} >
                         {
-                            objectData.children.length === 0
+                            this.objectData.children.length === 0
                             ? null
-                            : stageContent(objectData.children)
+                            : stageContent(this.objectData.children)
                         }
                     </div>
                 </div>
