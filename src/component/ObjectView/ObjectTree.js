27572e8d62cb5feb084c83b481090956a0f0c485
qs <350709533@qq.com>
2016-08-29 17:40:18 +0800

    对象树

--- src/component/ObjectView/ObjectTree.js
+++ src/component/ObjectView/ObjectTree.js
@@ -2,52 +2,84 @@
 import React from 'react';
 import $class from "classnames"
 
+import WidgetActions from '../../actions/WidgetActions';
+import WidgetStore from '../../stores/WidgetStore';
+
 class ObjectTree extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            nid : null
-            , openData : [1,3]
+            nid : null,
+            openData : [],
+            widgetTree: null,
+            selectedNode: [],
+            expandedNodes: [],
+            changed : null
         };
         this.chooseBtn = this.chooseBtn.bind(this);
         this.openBtn = this.openBtn.bind(this);
         this.closeBtn = this.closeBtn.bind(this);
-        this.objectData = {
-            showState:1,
-            openState:2,
-            nid:1,
-            children:[
-                {showState:0,openState:0,name:"111",images:"/dist/img/thumbnails.png",event:1,nid:2,children:[]}
-                , {showState:1,openState:2,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:3,
-                    children:[
-                        {showState:0,openState:0,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:4,children:[]}
-                        , {showState:1,openState:1,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:5,
-                            children:[
-                                {showState:0,openState:0,name:"333",images:"/dist/img/thumbnails.png",event:"",nid:6,children:[]}
-                            ]}
-                        , {showState:0,openState:0,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:7,children:[]}
-                    ]}
-                , {showState:0,openState:0,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:8,children:[]}
-                , {showState:1,openState:1,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:9,
-                    children:[
-                        {showState:0,openState:0,name:"222",images:"/dist/img/thumbnails.png",event:"",nid:10,children:[]}
-                        , {showState:1,openState:1,name:"222",images:"/dist/img/thumbnails.png",event:1,nid:11,
-                            children:[
-                                {showState:0,openState:1,name:"333",images:"/dist/img/thumbnails.png",event:"",nid:12,
-                                    children:[
-                                        {showState:1,openState:1,name:"4444444444444",images:"/dist/img/thumbnails.png",event:"",nid:14,
-                                            children:[
-                                                , {showState:1,openState:1,name:"55555555555555",images:"/dist/img/thumbnails.png",event:"",nid:15,
-                                                    children:[
-                                                        {showState:0,openState:0,name:"66666666666666",images:"/dist/img/thumbnails.png",event:"",nid:16,children:[]}
-                                                    ]}
-                                            ]}
-                                    ]}
-                            ]}
-                    ]}
-                , {showState:0,openState:0,name:"111",images:"/dist/img/thumbnails.png",event:"",nid:13,children:[]}
-            ]
+        this.onStatusChange = this.onStatusChange.bind(this);
+        this.onSelect = this.onSelect.bind(this);
+        this.onExpand = this.onExpand.bind(this);
+    }
+
+    componentDidMount() {
+        this.unsubscribe = WidgetStore.listen(this.onStatusChange);
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    onStatusChange(widget) {
+        //initTree : 初始化对象树
+        if (widget.initTree !== undefined){
+            this.setState({
+                widgetTree: widget.initTree[0]
+            });
         }
+
+        //redrawTree : 重新加载对象树
+        else if (widget.redrawTree !== undefined){
+            this.forceUpdate();
+        }
+
+        //selectWidget : 选择工具创建相应图层
+        if (widget.selectWidget !== undefined) {
+            let changed;
+            if (widget.selectWidget) {
+                let key = '' + widget.selectWidget.key;
+                changed = {selectedNode: [key], expandedNodes:this.state.expandedNodes};
+                let node = widget.selectWidget;
+                while (node) {
+                    key = '' + node.key;
+                    if (changed.expandedNodes.indexOf(key) < 0)
+                        changed.expandedNodes.push(key);
+                    node = node.parent;
+                }
+            } else {
+                changed = {selectedNode: []};
+            }
+            this.setState({
+                changed : changed
+            });
+        }
+    }
+
+    onSelect(selectedKeys, e) {
+        if (e.selectedNodes.length > 0) {
+            WidgetActions['selectWidget'](e.selectedNodes[0].props.node, true);
+        } else {
+            //WidgetActions['selectWidget'](null);
+        }
+    }
+
+    onExpand(expandedKeys, e) {
+        if (!e.expanded) {
+            expandedKeys.splice(expandedKeys.indexOf('' + e.node.props.node.key), 1);
+        }
+        this.setState({expandedNodes: expandedKeys});
     }
 
     chooseBtn(data){
@@ -58,65 +90,44 @@ class ObjectTree extends React.Component {
 
     openBtn(event){
         event.stopPropagation();
-        let id = event.currentTarget.getAttribute("data-nid");
+        let id = parseInt(event.currentTarget.getAttribute("data-nid"));
         let data = this.state.openData;
-        data.push(id);
-        this.setState({
-            openData : data
-        });
-        if( id == 1 ){
-            this.objectData.openState = 1;
-        }
-        else {
-            let fuc = ((v,i)=>{
-                if(v.nid == id){
-                    v.openState = 1;
-                    return;
-                }
-                if(v.children.length !== 0){
-                    v.children.map(fuc);
-                }
+        let index = data.indexOf(id);
+        if( index < 0){
+            data.push(id);
+            this.setState({
+                openData : data
             });
-            this.objectData.children.map(fuc);
         }
-        //console.log(data);
     }
 
     closeBtn(event){
         event.stopPropagation();
-        let id = event.currentTarget.getAttribute("data-nid");
+        let id = parseInt(event.currentTarget.getAttribute("data-nid"));
         let data = this.state.openData;
         let index = data.indexOf(id);
-        data.splice(index, 1);
-        this.setState({
-            openData : data
-        });
-        if(id == 1){
-            this.objectData.openState = 2;
-        }
-        else {
-            let fuc = ((v,i)=>{
-                if(v.nid == id){
-                    v.openState = 2;
-                    return;
-                }
-                if(v.children.length !== 0){
-                    v.children.map(fuc);
-                }
+        if( index >= 0){
+            data.splice(index, 1);
+            this.setState({
+                openData : data
             });
-            this.objectData.children.map(fuc);
         }
-        //console.log(data);
     }
 
     render() {
+        //console.log(this.state.widgetTree);
+        let objectData = this.state.widgetTree;
         let num = 0;
 
         let btn = (show)=>{
-            //0图层及图层内的所有内容不可看，1可在舞台看见
+            //0图层及图层内的所有内容不可看，1可在舞台看见，-1指的是整个舞台，必是可见的
             if(show === 0 ){
                 return <div className="btn f--hcc hide-btn"><span /></div>;
-            }else{
+            }
+            else if(show === -1){
+                return <div className="btn f--hcc show-btn"><span /></div>;
+            }
+            else{
                 return <div className="btn f--hcc show-btn"><span /></div>;
             }
         };
@@ -127,10 +138,12 @@ class ObjectTree extends React.Component {
                 return <span className="icon not-icon" />;
             }
             else if(open === 1){
-                return <span className="icon close-icon" onClick={this.closeBtn.bind(this)} data-nid={nid} />;
-            }
-            else {
-                return <span className="icon open-icon" onClick={this.openBtn.bind(this)} data-nid={nid} />;
+                if( this.state.openData.indexOf(nid) >= 0){
+                    return <span className="icon open-icon" onClick={this.closeBtn.bind(this)} data-nid={nid} />;
+                }
+                else {
+                    return <span className="icon close-icon" onClick={this.openBtn.bind(this)} data-nid={nid} />;
+                }
             }
         };
 
@@ -142,16 +155,28 @@ class ObjectTree extends React.Component {
         };
 
         let fuc = (v,i)=>{
+            //console.log(v);
             return  <div className="item" key={i}>
-                        <div className={$class("item-title f--h f--hlc",{"active": v.nid === this.state.nid})}
-                             onClick={this.chooseBtn.bind(this,v.nid)}
+                        <div className={$class("item-title f--h f--hlc",{"active": v.key === this.state.nid})}
+                             onClick={this.chooseBtn.bind(this,v.key)}
                              style={{ paddingLeft: num === 0 ? "28px" :num *20 + 22 +"px" }}>
-                            { btn(v.showState) }
-                            { icon(v.openState, v.nid) }
-                            <img src={v.images} />
-                            <p>{v.name}</p>
+
+                            { btn(1) }
+
                             {
-                                v.event
+                                v.children.length > 0
+                                    ? icon( 1 , v.key)
+                                    : icon( 0 , v.key)
+                            }
+
+                            {
+                                <img src="/dist/img/thumbnails.png" />
+                            }
+
+                            <p>{v.className}</p>
+
+                            {
+                                Object.keys(v.events).length > 0
                                     ? <span className="event-icon" />
                                     : null
                             }
@@ -169,27 +194,34 @@ class ObjectTree extends React.Component {
 
         return (
             <div className="ObjectTree">
-                <div className="stage">
-                    <div className={$class("stage-title f--h f--hlc",{"active": this.objectData.nid == this.state.nid})}
-                         onClick={this.chooseBtn.bind(this,this.objectData.nid)}>
-                        { btn(this.objectData.showState) }
-                        { icon(this.objectData.openState, this.objectData.nid) }
-                        <span className="stage-icon" />
-                        <p>舞台</p>
-                    </div>
+                {
+                    !objectData
+                    ? null
+                    : <div className="stage">
+                        <div className={$class("stage-title f--h f--hlc",{"active": objectData.tree.key === this.state.nid})}
+                             onClick={this.chooseBtn.bind(this, objectData.tree.key)}>
+                            { btn(-1) }
+                            {
+                                objectData.tree.children.length > 0
+                                    ? icon( 1 , objectData.tree.key)
+                                    : icon( 0 , objectData.tree.key)
+                            }
+                            <span className="stage-icon" />
+                            <p>{ objectData.name }</p>
+                        </div>
 
-                    <div className={$class("stage-content", {"hidden": this.objectData.openState === 0 || this.objectData.openState === 1 })} >
-                        {
-                            this.objectData.children.length === 0
-                            ? null
-                            : stageContent(this.objectData.children)
-                        }
+                        <div className={$class("stage-content", {"hidden":  this.state.openData.indexOf(objectData.tree.key) < 0 })} >
+                            {
+                                objectData.tree.children.length === 0
+                                    ? null
+                                    : stageContent(objectData.tree.children)
+                            }
+                        </div>
                     </div>
-                </div>
+                }
             </div>
         );
     }
 }
 
 module.exports = ObjectTree;
-
