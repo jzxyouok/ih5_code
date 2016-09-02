62cef59f53d6a0d8fd57514a14be76ffc194dae5
tianlai_huang <chanmaozha@gmail.com>
2016-09-02 15:38:56 +0800

    fix: stage mouse wheel scroll, enable for Firefox

--- src/component/DesignView.js
+++ src/component/DesignView.js
@@ -2,24 +2,27 @@ import React from 'react';
 
 class DesignView extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.scroll = this.scroll.bind(this);
+    }
+
     componentDidMount() {
-        this.refs.container.addEventListener('mousewheel', this.scroll.bind(this));
-        this.refs.container.addEventListener('DOMMouseScroll', this.scroll.bind(this));
     }
 
     componentWillUnmount() {
-        this.refs.container.removeEventListener('mousewheel', this.scroll.bind(this));
-        this.refs.container.removeEventListener('DOMMouseScroll', this.scroll.bind(this));
     }
 
     scroll(event) {
+        event.preventDefault();
+        event.stopPropagation();
         //console.log(event);
         let y  = event.deltaY;
         let x  = event.deltaX;
         if(y===0 || y===-0) return;
         let top = window.getComputedStyle(this.refs.view,null).getPropertyValue("top");
         let t = parseFloat(top.replace(/(px)?/, ''));
-        t = t - y/2;
+        t = t - y / 10;
         this.refs.view.style.top = t+'px';
     }
 
@@ -27,7 +30,7 @@ class DesignView extends React.Component {
       return (
         <div className='f--hcc' 
             id='DesignView-Container'
-            ref='container'>
+            ref='container' onWheel={this.scroll}>
           <div id='canvas-dom' className="DesignView" ref='view'></div>
         </div>
       );
