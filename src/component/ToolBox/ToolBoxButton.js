0ee249349d16f5450112f890c9ca17e3b784463e
brianHuang <huangxiaobin@ih5.cn>
2016-09-13 13:48:05 +0800

    添加点击按钮添加图形的东东

--- src/component/ToolBox/ToolBoxButton.js
+++ src/component/ToolBox/ToolBoxButton.js
@@ -1,6 +1,7 @@
 import React,{PropTypes, Component} from 'react';
 import JSZip from 'jszip';
 import cls from 'classnames';
+import $ from 'jquery';
 import WidgetActions from '../../actions/WidgetActions';
 import ToolBoxAction from '../../actions/ToolBoxAction';
 import ToolBoxStore, {isActiveButton} from '../../stores/ToolBoxStore';
@@ -91,8 +92,102 @@ class ToolBoxButton extends Component {
                 }
             });
         } else {
-            WidgetActions['addWidget'](this.props.className, this.props.param);
+            if (this.props.className === 'rect') {
+                this.onDrawRect().then(data => {
+                    //可在这设置弹框等内容
+                    this.props.param.shapeWidth = parseInt(data.shapeWidth);
+                    this.props.param.shapeHeight = parseInt(data.shapeHeight);
+                    WidgetActions['addWidget'](this.props.className, this.props.param);
+                });
+            } else {
+                WidgetActions['addWidget'](this.props.className, this.props.param);
+            }
         }
+    }
+
+    onDrawRect() {
+        let startX = 0, startY = 0;
+        let retcLeft = "0px", retcTop = "0px", retcHeight = "0px", retcWidth = "0px";
+        let flag = false;
+        var def = $.Deferred();
+
+        var mouseDown = e => {
+            e.preventDefault();
+            e.stopPropagation();
+            flag = true;
+            try{
+                var evt = window.event || e;
+                var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+                var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+                startX = evt.clientX + scrollLeft;
+                startY = evt.clientY + scrollTop;
+                var div = document.createElement("div");
+                div.id = 'drawRect';
+                div.className = "div";
+                div.style.position = "absolute";
+                div.style.left = startX + "px";
+                div.style.top = startY + "px";
+                div.style.border = '1px dotted white';
+                document.body.appendChild(div);
+            }catch(e){
+                //alert(e);
+            }
+        };
+
+        var mouseUp = e => {
+            e.preventDefault();
+            e.stopPropagation();
+            try{
+                var drawRectDiv = document.getElementById('drawRect');
+                document.body.removeChild(drawRectDiv);
+                def.resolve({shapeWidth:retcWidth,shapeHeight:retcHeight});
+                removeDrawEventListner();
+            }catch(e){
+                //alert(e);
+            }
+            flag = false;
+        };
+
+        var mouseMove = e => {
+            e.preventDefault();
+            e.stopPropagation();
+            if(flag){
+                try{
+                    var evt = window.event || e;
+                    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+                    var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+                    var drawRectDiv = document.getElementById('drawRect');
+                    retcLeft = (startX - evt.clientX - scrollLeft > 0 ? evt.clientX + scrollLeft : startX) + "px";
+                    retcTop = (startY - evt.clientY - scrollTop > 0 ? evt.clientY + scrollTop : startY) + "px";
+                    retcHeight = Math.abs(startY - evt.clientY - scrollTop) + "px";
+                    retcWidth = Math.abs(startX - evt.clientX - scrollLeft) + "px";
+                    drawRectDiv.style.left = retcLeft;
+                    drawRectDiv.style.top = retcTop;
+                    drawRectDiv.style.width = retcWidth;
+                    drawRectDiv.style.height = retcHeight;
+                }catch(e){
+                    //alert(e);
+                }
+            }
+        };
+
+        var addDrawEventListener = () => {
+            document.body.style.cursor = 'crosshair';
+            document.body.addEventListener('mousedown', mouseDown);
+            document.body.addEventListener('mouseup', mouseUp);
+            document.body.addEventListener('mousemove', mouseMove);
+        };
+
+        var removeDrawEventListner = () => {
+            document.body.style.cursor = 'auto';
+            document.body.removeEventListener('mousedown', mouseDown);
+            document.body.removeEventListener('mouseup', mouseUp);
+            document.body.removeEventListener('mousemove', mouseMove);
+        };
+
+        addDrawEventListener();
+
+        return def.promise();
     }
 
     onRightClick(event) {
