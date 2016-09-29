66fcfb3124d04d54bbf9db01cc0e9b6368936e9a
brianHuang <huangxiaobin@ih5.cn>
2016-09-29 15:01:51 +0800

    mini change

--- src/component/FunctionView/FunctionView.js
+++ src/component/FunctionView/FunctionView.js
@@ -74,8 +74,8 @@ class FunctionView extends React.Component {
     }
 
     render() {
-        return <div id="FunctionView"
-                    className={$class({'keep':this.state.minSize}, {'hidden':this.props.isHidden})}
+        return <div id="FuncView"
+                    className={$class('FuncView',{'keep':this.state.minSize}, {'hidden':this.props.isHidden})}
                     style={{ left : this.props.expanded? '65px':'37px'}}>
             <div id='FunctionViewHeader' className="f--hlc">
                 <span className="flex-1">函数</span>
