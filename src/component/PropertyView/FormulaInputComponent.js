fdff732cee0498a8f6e1bcd41b9869a70a2be627
brianHuang <huangxiaobin@ih5.cn>
2016-10-20 12:17:58 +0800

    update

--- src/component/PropertyView/FormulaInputComponent.js
+++ src/component/PropertyView/FormulaInputComponent.js
@@ -9,6 +9,7 @@ import { Input } from 'antd';
 
 import WidgetStore from '../../stores/WidgetStore'
 import WidgetActions from '../../actions/WidgetActions'
+import { SelectTargetButton } from '../PropertyView/SelectTargetButton';
 
 const inputType = {
     value: 1,
@@ -23,7 +24,6 @@ class FormulaInput extends React.Component {
         this.state = {
             value: null,
             currentType: inputType.value,
-            // didActiveSelectTargetMode: false,
             objectDropDownVisible: false, //对象dropdown
             propertyDropDownVisible: false, //属性dropdown
             objectList: []
@@ -75,46 +75,8 @@ class FormulaInput extends React.Component {
             this.setState({
                 objectList: widget.allWidgets
             });
-        // } else if (widget.didSelectEventTarget) {
-        //     if(widget.didSelectEventTarget.target&&this.state.didActiveSelectTargetMode) {
-        //         let target = widget.didSelectEventTarget.target;
-        //         let getTarget = false;
-        //         this.state.objectList.forEach((v)=>{
-        //             if(target.key === v.key){
-        //                 getTarget = true;
-        //             }
-        //         });
-        //         if (getTarget) {
-        //             console.log('getValueHere');
-        //             this.setState({
-        //                 didActiveSelectTargetMode: false
-        //             }, ()=> {
-        //                 WidgetActions['eventSelectTargetMode'](false, {formulaInput:true});
-        //             });
-        //         }
-        //     }
         }
     }
-
-    // onActiveSelectTargetMode(e) {
-    //     e.stopPropagation();
-    //     this.setState({
-    //         didActiveSelectTargetMode: !this.state.didActiveSelectTargetMode,
-    //         objectDropDownVisible: false
-    //     }, ()=>{
-    //         // WidgetActions['eventSelectTargetMode'](this.state.didActiveSelectTargetMode, {formulaInput:true});
-    //     });
-    // }
-
-    // onSelectTargetModeBlur(e) {
-    //     if(this.state.didActiveSelectTargetMode) {
-    //         this.setState({
-    //             didActiveSelectTargetMode: false,
-    //         }, ()=>{
-    //             // WidgetActions['eventSelectTargetMode'](false, {formulaInput:true});
-    //         });
-    //     }
-    // }
 
     onObjectVisibleChange(flag){
         this.setState({
@@ -122,8 +84,8 @@ class FormulaInput extends React.Component {
         })
     }
 
-    onObjectSelect(){
-
+    onObjectSelect(object){
+        console.log(object);
     }
 
     onInputTypeValueChange(e) {
@@ -161,10 +123,9 @@ class FormulaInput extends React.Component {
                     <div className={$class("formula--dropDown")}
                         style={{minWidth:this.minWidth}}>
                         <div className="formula-title f--hlc">
-                            {/*<button className={$class('formula-object-icon', {'active':this.state.didActiveSelectTargetMode})}*/}
-                                    {/*onClick={this.onActiveSelectTargetMode}/>*/}
-                            {/*var formularInputId = 0;*/}
-                            {/*fid: 'formularInput'+ formularInputId++   //标示＋控制*/}
+                            <SelectTargetButton className={'formula-object-icon'}
+                                                disabled={false}
+                                                getResult={this.onObjectSelect} />
                             <Input placeholder="比较值／对象" value={this.state.value} onChange={this.onInputTypeValueChange.bind(this)}/>
                             <span className="value-right-icon" />
                         </div>
