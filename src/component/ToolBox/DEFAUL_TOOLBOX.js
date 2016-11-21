44093f8c7df4497357877bae0bf509530c9dff49
brianHuang <huangxiaobin@ih5.cn>
2016-11-21 11:44:44 +0800

    pageContainer新建的时候clip属性默认设为是

--- src/component/ToolBox/DEFAUL_TOOLBOX.js
+++ src/component/ToolBox/DEFAUL_TOOLBOX.js
@@ -186,7 +186,7 @@ var DEFAULT_TOOLBOX = {
         primary: 0,
         secondary: [
             {cid:TOOL_ID.PAGE,name:'页面',icon:pageIcon, className:'page'},
-            {cid:TOOL_ID.PAGECONTAINER,name:'页面容器',icon:pageContainerIcon, className:'pagecontainer', param: {'swipeType': 4,'width':200,'height':200}}
+            {cid:TOOL_ID.PAGECONTAINER,name:'页面容器',icon:pageContainerIcon, className:'pagecontainer', param: {'swipeType': 4,'width':200,'height':200,'clipped':true}}
         ]
     },{
         name:'容器',
